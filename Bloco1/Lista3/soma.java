8d46ad685c0bdda8742f7427b5132752942dadf8
@Bruna <88908448+bruna41@users.noreply.github.com>
2021-11-22 17:26:04 -0300

    ECjava

---
+++ Bloco1/Lista3/soma.java
@@ -0,0 +1,18 @@
+package lista3;
+
+import java.util.Scanner;
+
+public class soma {
+
+	public static void main(String[] args) {
+		int num, sT = 0;
+		Scanner rec = new Scanner (System.in);	
+		do {
+			System.out.println("N�meros: ");
+			num = rec.nextInt();
+			sT += num;
+		}while (num != 0);
+		System.out.println(sT);
+	}
+
+}
