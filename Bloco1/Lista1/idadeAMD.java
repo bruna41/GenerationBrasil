0df008a91e94d5a4ad7d73352fe9a0d380a6b925
@Bruna <88908448+bruna41@users.noreply.github.com>
2021-11-22 17:08:06 -0300

    Lóg.Java

---
+++ Bloco1/Lista1/idadeAMD.java
@@ -0,0 +1,19 @@
+package lista1;
+
+import java.util.Scanner;
+
+public class idadeEmAMD {
+
+	public static void main(String[] args) {
+		int idadeD, idadeA, idadeM;
+		System.out.print("Digite sua idade em dias: ");
+		Scanner id = new Scanner (System.in);
+		idadeD = id.nextInt();
+		
+		idadeA =  idadeD / 365;
+		idadeM = (idadeD % 365) / 30;
+		
+		System.out.println("Sua idade em anos, meses e dias �, respectivamente: " + idadeA + ", " + idadeM + ", " + idadeD);
+	}
+
+}
