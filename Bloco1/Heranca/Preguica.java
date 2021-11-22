b3b37eddb3a66f29eaf70f7cbcd428cfcfc30058
@Bruna <88908448+bruna41@users.noreply.github.com>
2021-11-22 17:44:51 -0300

    her.Java

---
+++ Bloco1/Heranca/Preguica.java
@@ -0,0 +1,16 @@
+package POO2;
+
+public class Preguica extends Animal {
+	
+	@Override
+    public void correr () {
+        System.out.println("Subindo...");
+    }
+
+	@Override
+	public void som() {
+		System.out.println("Hummmm...");
+		
+	}
+
+}
