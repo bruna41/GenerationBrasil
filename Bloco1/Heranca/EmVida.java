c78234cff0d189f62b37094cb9d2aaf1c993203a
@Bruna <88908448+bruna41@users.noreply.github.com>
2021-11-22 17:43:47 -0300

    her.Java

---
+++ Bloco1/Heranca/EmVida.java
@@ -0,0 +1,16 @@
+package POO2;
+
+public class EmVida {
+
+	public static void main(String[] args) {
+		
+		Cavalo c = new Cavalo ();
+		c.som();
+		Cachorro cao = new Cachorro ();
+		cao.som();
+		Pregui�a p = new Pregui�a ();
+		p.som();
+
+	}
+
+}
