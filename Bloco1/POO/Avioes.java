58cd5ab6bf8b2a3c4c1785fc20c3d2e5f186577c
@Bruna <88908448+bruna41@users.noreply.github.com>
2021-11-22 17:35:27 -0300

    POO

---
+++ Bloco1/POO/Avioes.java
@@ -0,0 +1,11 @@
+package POO;
+
+public class Avioes {
+
+	public static void main(String[] args) {
+		Aviao aviao1 = new Aviao(300, 24, 1000);
+		System.out.println("O avi�o est� h� " + aviao1.getAtributos() + "km/h");
+
+	}
+
+}
