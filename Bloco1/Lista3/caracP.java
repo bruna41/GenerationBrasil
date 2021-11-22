1a98d7a490cc6b973dba3764558a9397027db3a5
@Bruna <88908448+bruna41@users.noreply.github.com>
2021-11-22 17:21:27 -0300

    ECjava

---
+++ Bloco1/Lista3/caracP.java
@@ -0,0 +1,31 @@
+package lista3;
+
+import java.util.Scanner;
+
+public class caracP {
+
+	public static void main(String[] args) {
+		int idade, pop = 0, somaPC = 0;
+		String f, m, o, pC, pN, pA;
+		Scanner c = new Scanner (System.in);
+		while (pop <= 2) {
+			System.out.println("Sua idade: ");
+			idade = c.nextInt();
+			System.out.println("Seu sexo: ");
+			f = c.nextLine();
+			m = c.nextLine();
+			o = c.nextLine();
+			System.out.println("Vc � uma pessoa calma? (s/n): ");
+			pC = c.nextLine();
+			System.out.println("Vc � uma pessoa nervosa? (s/n): ");
+			pN = c.nextLine();
+			System.out.println("Vc � uma pessoa agressiva? (s/n): ");
+			pA = c.nextLine();
+			if (pC == "s") {
+				somaPC ++;
+			}
+		}
+		System.out.println(somaPC);
+	}
+
+}
