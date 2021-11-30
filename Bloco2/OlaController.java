f00bfe56b0267bd3d6ab5728ff19c8f5d2251b0c
@Bruna <88908448+bruna41@users.noreply.github.com>
2021-11-30 17:49:11 -0300

    exerc.Spring

---
+++ Bloco2/OlaController.java
@@ -0,0 +1,17 @@
+package com.helloworld.hello.controlller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class OlaController {
+
+	@GetMapping
+	public String hallo () {
+		return "Olá, mundo!!";
+	}
+	
+	
+}
